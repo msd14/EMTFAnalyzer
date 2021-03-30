dd423ddabc9da103e6e41cc6a53a72bf1cc6b291
Sven Dildick <sven.dildick@cern.ch>
2021-03-30 14:23:41 -0500

    Add more matchers and analyzers

--- NTupleMaker/test/Run3_MC_NTuple.py
+++ NTupleMaker/test/Run3_MC_NTuple.py
@@ -12,6 +12,8 @@ from Configuration.Eras.Era_Run3_cff import Run3
 
 process = cms.Process('NTUPLE', Run3)
 
+matchGEM = False
+
 ## Import standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
@@ -35,8 +37,7 @@ process.load("RecoMuon.TrackingTools.MuonServiceProxy_cff")
 process.load("RecoMuon.TrackingTools.MuonTrackLoader_cff")
 
 ## Message Logger and Event range
-#process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False))
 
 ## Global Tags
@@ -50,7 +51,8 @@ readFiles = cms.untracked.vstring()
 process.source = cms.Source(
     'PoolSource',
     fileNames = cms.untracked.vstring(
-        'file:step2bis_run3.root'
+        '/store/user/mdecaro/SingleMu_Run3_Pt1to50OneOverPt_noPU_10M/CRAB3_SingleMu_Run3_Pt1to50OneOverPt_noPU_10M/210325_152507/0000/step1_85.root'
+        #    'file:step2bis_run3.root'
     ),
 )
 
@@ -60,34 +62,68 @@ process.source = cms.Source(
 process.load('EMTFAnalyzer.NTupleMaker.GEMEMTFMatcher_cfi')
 process.load('EMTFAnalyzer.NTupleMaker.FlatNtuple_cfi')
 
-process.GEMEMTFMatcher.emtfHitTag = cms.InputTag("simEmtfDigis","","ReL1")
-process.GEMEMTFMatcher.emtfTrackTag = cms.InputTag("simEmtfDigis","","ReL1")
+## Run-2 matcher
+process.GEMEMTFMatcherMCRun2 = process.GEMEMTFMatcher.clone()
+process.GEMEMTFMatcherMCRun2.emtfHitTag = cms.InputTag("simEmtfDigis")
+process.GEMEMTFMatcherMCRun2.emtfTrackTag = cms.InputTag("simEmtfDigis")
+
+## Run-3 matcher
+process.GEMEMTFMatcher.emtfHitTag = cms.InputTag("simEmtfDigisRun3CCLUT")
+process.GEMEMTFMatcher.emtfTrackTag = cms.InputTag("simEmtfDigisRun3CCLUT")
 
 ## just in case the copads need to be rerun
 #process.GEMEMTFMatcher.gemCoPadTag = cms.InputTag("cscTriggerPrimitiveDigis","","NTUPLE")
 
+## to be sure that the GEMEMTFMatcher is not screwing up any of the existing EMTFHit or EMTFTrack
+## collections, run 4 ntuplizers:
+##   Run-2
+##   Run-2 + GEM
+##   Run-3
+##   Run-3 + GEM
+
+##   Run-2
 process.FlatNtupleMCRun2 = process.FlatNtupleMC.clone()
 process.FlatNtupleMCRun2.emtfHitTag = cms.InputTag("simEmtfDigis")
 process.FlatNtupleMCRun2.emtfTrackTag = cms.InputTag("simEmtfDigis")
 
+##   Run-2 + GEM
+process.FlatNtupleMCRun2GEM = process.FlatNtupleMC.clone()
+process.FlatNtupleMCRun2GEM.emtfHitTag = cms.InputTag("GEMEMTFMatcherMCRun2")
+process.FlatNtupleMCRun2GEM.emtfTrackTag = cms.InputTag("GEMEMTFMatcherMCRun2")
+
+##   Run-3
 process.FlatNtupleMC.emtfHitTag = cms.InputTag("simEmtfDigisRun3CCLUT")
 process.FlatNtupleMC.emtfTrackTag = cms.InputTag("simEmtfDigisRun3CCLUT")
 
-process.matcher = cms.Sequence(process.cscTriggerPrimitiveDigis * process.GEMEMTFMatcher)
+##   Run-3 + GEM
+process.FlatNtupleMCGEM = process.FlatNtupleMC.clone()
+process.FlatNtupleMCGEM.emtfHitTag = cms.InputTag("GEMEMTFMatcher")
+process.FlatNtupleMCGEM.emtfTrackTag = cms.InputTag("GEMEMTFMatcher")
 
-process.Analysis = cms.Sequence(process.FlatNtupleMCRun2 * process.FlatNtupleMC)
+process.matcher = cms.Sequence(
+    process.GEMEMTFMatcherMCRun2 *
+    process.GEMEMTFMatcher
+)
+
+process.Analysis = cms.Sequence(
+    process.FlatNtupleMCRun2 *
+    process.FlatNtupleMCRun2GEM *
+    process.FlatNtupleMC *
+    process.FlatNtupleMCGEM
+)
 
 process.Analysis_step = cms.Path(
-    #process.matcher *
-    process.Analysis)
+    process.matcher *
+    process.Analysis
+)
 
 process.endjob_step = cms.EndPath(process.endOfProcess)
 
 ## NTuple output File
 process.TFileService = cms.Service(
     "TFileService",
-    fileName = cms.string('EMTF_MC_NTuple_SingleMu_20201202.root')
-    )
+    fileName = cms.string('EMTF_MC_NTuple_SingleMu.root')
+)
 
 # Schedule definition
 process.schedule = cms.Schedule(process.Analysis_step,process.endjob_step)
